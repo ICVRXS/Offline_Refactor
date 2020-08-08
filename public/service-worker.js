b383eaa54c279daf2ad580a0ab3e0c2296287cac
ICVRXS <calebhay@rocketmail.com>
2020-08-07 20:01:04 -0700

    Fixed syntax in service-worker.js which prevented it from functioning properly

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,12 +2,13 @@ const FILES_TO_CACHE = [
     "/",
     "/index.html",
     "/styles.css",
-    "index.js",
-    "icon-192x192.png",
-    "icon-512x512.png"
+    "/index.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/db.js"
 ];
 
-const CACHE_NAME = "static-cache-v2";
+const CACHE_NAME = "budget-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 self.addEventListener("install", function(evt) {
